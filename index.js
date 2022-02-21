de3ae173323b16cdeeb8e418418fe0872c731765
mraditya89 <yudi.aditya.89@gmail.com>
2022-02-21 16:03:58 +0700

    added jwt-parse

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const jwt = require("jsonwebtoken");
 
 const PORT = process.env.PORT || 5000;
 const HOSTNAME = process.env.HOSTNAME || "localhost";
+const secret = "s3Cr3T";
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -21,10 +22,20 @@ app.post("/scan", async (req, res) => {
   body["T"] = new Date().getTime();
 
   const data = JSON.stringify(body);
-  const secret = "s3Cr3T";
+
   const token = jwt.sign(body, secret, { expiresIn: "12h" });
   const qrCodeSrc = await qrCode.toDataURL(token);
   res.render("scan", { qrCode: qrCodeSrc, data, token, secret });
+});
+
+app.get("/jwt-parse", async (req, res) => {
+  try {
+    let { query } = req;
+    const decoded = await jwt.verify(query.token, secret);
+    return res.send({ result: decoded });
+  } catch (error) {
+    return res.status(500).send({ error: error.message });
+  }
 });
 
 app.listen(PORT, HOSTNAME, () => {
